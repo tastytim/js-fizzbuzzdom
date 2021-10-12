2b8b9d083aead8797f5456dc9b8c4e60dd90d9ca
tastytim <tastytimgm@gmail.com>
2021-10-12 17:48:21 +0200

    commit 2

--- app.js
+++ app.js
@@ -1,50 +1,34 @@
-
-// Prendo id quadretti e aggiungo classe container-fluid
 const quadrettiContainer = document.getElementById("quadretti-container");
-quadrettiContainer.classList.add("container-fluid");
-
-
-// Creo div e aggiungo classi "row" "col-rows-7"
+quadrettiContainer.classList.add("my-container");
 const row = document.createElement("div");
-row.classList.add("row","col-row-7");
+row.classList.add("my-row");
+quadrettiContainer.append(row);
 
-// Aggiungo la row al container-fluid e container-fluid al quadretti-container
-quadrettiContainer.append(container);
-container.append(row);
+for (let i = 1; i <= 100; i++) {
+  const col = document.createElement("div");
+  col.classList.add("my-col");
 
+  const quadretto = document.createElement("div");
+  quadretto.classList.add("my-box", "text-center");
 
-for(let i = 1; i <=100; i++){
-    // Creo colonna
-    const col = document.createElement("div");
-    col.classList.add("col");
+  const text = document.createElement("text");
+  text.classList.add("line-h-200");
 
+  if (i % 3 === 0 && i % 5 === 0) {
+    quadretto.classList.add("red");
+    text.textContent = "fizzbuzz";
+  } else if (i % 3 === 0 && i % 5 != 0) {
+    quadretto.classList.add("green");
+    text.textContent = "fizz";
+  } else if (i % 5 === 0 && i % 3 != 0) {
+    quadretto.classList.add("yellow");
+    text.textContent = "buzz";
+  } else {
+    quadretto.classList.add("blue");
+    text.textContent = i;
+  }
 
-    // Creo quadretto con ratio 1x1
-    const quadretto = document.createElement("div");
-    quadretto.classList.add("ratio", "ratio-1x1");
-
-//    Aggiungo colore e testo al quadretto
-    if(i % 3 === 0 && i % 5 === 0){
-        quadretto.style.background = "red";
-        quadretto.textContent = "fizzbuzz";    
-    }else if(i % 3 === 0){
-        quadretto.style.background = "green";
-        quadretto.textContent = "fizz";
-    }else if(i % 5 === 0){
-        quadretto.style.background = "yellow";
-        quadretto.textContent = "buzz";
-    }else{ 
-        quadretto.style.background = "blue";
-        quadretto.textContent = i;
-    }
-
-    // Inserisco al interno della colonna quadretto
-    col.append(quadretto);
-
-    // Inserisco colonna al row
-    row.append("col");
-    
+  quadretto.append(text);
+  col.append(quadretto);
+  row.append(col);
 }
-    
-
-
